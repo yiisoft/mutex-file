2c74f73e7298c1dee51ccc16c09ab17957aa09c8
Alexander Makarov <sam@rmcreative.ru>
2021-08-01 18:28:41 +0300

    Use MutexTestTrait

--- tests/FileMutexTest.php
+++ tests/FileMutexTest.php
@@ -6,56 +6,11 @@ namespace Yiisoft\Mutex\File\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Mutex\File\FileMutex;
+use Yiisoft\Mutex\Tests\MutexTestTrait;
 
 final class FileMutexTest extends TestCase
 {
-    public function testMutexAcquire(): void
-    {
-        $mutex = $this->createMutex('testMutexAcquire');
-
-        $this->assertTrue($mutex->acquire());
-        $mutex->release();
-    }
-
-    public function testThatMutexLockIsWorking(): void
-    {
-        $mutexOne = $this->createMutex('testThatMutexLockIsWorking');
-        $mutexTwo = $this->createMutex('testThatMutexLockIsWorking');
-
-        $this->assertTrue($mutexOne->acquire());
-        $this->assertFalse($mutexTwo->acquire());
-        $mutexOne->release();
-        $mutexTwo->release();
-
-        $this->assertTrue($mutexTwo->acquire());
-        $mutexTwo->release();
-    }
-
-    public function testThatMutexLockIsWorkingOnTheSameComponent(): void
-    {
-        $mutex = $this->createMutex('testThatMutexLockIsWorkingOnTheSameComponent');
-
-        $this->assertTrue($mutex->acquire());
-        $this->assertFalse($mutex->acquire());
-
-        $mutex->release();
-        $mutex->release();
-    }
-
-    public function testTimeout(): void
-    {
-        $mutexName = __FUNCTION__;
-        $mutexOne = $this->createMutex($mutexName);
-        $mutexTwo = $this->createMutex($mutexName);
-
-        $this->assertTrue($mutexOne->acquire());
-        $microtime = microtime(true);
-        $this->assertFalse($mutexTwo->acquire(1));
-        $diff = microtime(true) - $microtime;
-        $this->assertTrue($diff >= 1 && $diff < 2);
-        $mutexOne->release();
-        $mutexTwo->release();
-    }
+    use MutexTestTrait;
 
     /**
      * @throws \RuntimeException
